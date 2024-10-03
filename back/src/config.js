4ce10b25930d9504e7e02267aea13f012fd44a72
Benjuxx2303 <113943385+Benjuxx2303@users.noreply.github.com>
2024-10-03 18:06:22 -0300

    avance

--- back/src/config.js
+++ back/src/config.js
@@ -3,6 +3,7 @@ import {config} from "dotenv";
 config()
 
 export const PORT= process.env.PORT || 3000;
+export const HOST= process.env.HOST || 'localhost';
 
 export const DB_HOST= process.env.DB_HOST || 'localhost';
 export const DB_PORT= process.env.DB_PORT || 3306;

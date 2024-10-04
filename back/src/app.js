55b329d545be680275e223f53eeb03036b604be2
Fabl23 <fabiandiaztorres13@gmail.com>
2024-10-04 16:31:33 -0300

    cors

--- back/src/app.js
+++ back/src/app.js
@@ -1,27 +1,31 @@
 import express from "express";
 import morgan from "morgan";
 // TODO: import desde routes
+import cors from 'cors';
 import indexRoutes from './routes/index.routes.js';
 
-import rol_personalRoutes from './routes/rol_personal.routes.js';
-import companiaRoutes from './routes/compania.routes.js';
-import personalRoutes from './routes/personal.routes.js';
-import claveRoutes from './routes/clave.routes.js';
-import tipo_maquinaRoutes from './routes/tipo_maquina.routes.js';
-import procedenciaRoutes from './routes/procedencia.routes.js';
-import maquinaRoutes from './routes/maquina.routes.js';
-import tallerRoutes from './routes/taller.routes.js';
-import conductor_maquinaRoutes from './routes/conductor_maquina.routes.js';
+
 import bitacoraRoutes from './routes/bitacora.routes.js';
-import mantencionRoutes from './routes/mantencion.routes.js';
-import tipoMantencionRoutes from './routes/tipo_mantencion.routes.js';
-import usuarioRoutes from './routes/usuario.routes.js';
-import divisionRoutes from './routes/division.routes.js';
-import subdivisionRoutes from './routes/subdivision.routes.js';
-import servicioRoutes from './routes/servicio.routes.js';
+import claveRoutes from './routes/clave.routes.js';
+import companiaRoutes from './routes/compania.routes.js';
+import conductor_maquinaRoutes from './routes/conductor_maquina.routes.js';
 import detalle_mantencionRoutes from './routes/detalle_mantencion.routes.js';
+import divisionRoutes from './routes/division.routes.js';
+import mantencionRoutes from './routes/mantencion.routes.js';
+import maquinaRoutes from './routes/maquina.routes.js';
+import personalRoutes from './routes/personal.routes.js';
+import procedenciaRoutes from './routes/procedencia.routes.js';
+import rol_personalRoutes from './routes/rol_personal.routes.js';
+import servicioRoutes from './routes/servicio.routes.js';
+import subdivisionRoutes from './routes/subdivision.routes.js';
+import tallerRoutes from './routes/taller.routes.js';
+import tipoMantencionRoutes from './routes/tipo_mantencion.routes.js';
+import tipo_maquinaRoutes from './routes/tipo_maquina.routes.js';
+import usuarioRoutes from './routes/usuario.routes.js';
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use(morgan('dev'));
 

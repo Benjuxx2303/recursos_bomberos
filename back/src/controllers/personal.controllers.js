c4938d0130d881b38e5844ab8c9a12849a99b139
Fabl23 <fabiandiaztorres13@gmail.com>
2024-12-03 02:32:41 -0300

    Añade archivo .env.example al .gitignore y elimina el archivo de ejemplo. Agrega el cálculo de antigüedad en la consulta de personal por ID.

--- back/src/controllers/personal.controllers.js
+++ back/src/controllers/personal.controllers.js
@@ -122,7 +122,8 @@ export const getPersonalbyID = async (req, res) => {
                    DATE_FORMAT(p.fec_ingreso, '%d-%m-%Y') AS fec_ingreso,
                    p.img_url, p.obs, p.isDeleted,
                    rp.nombre AS rol_personal, 
-                   c.nombre AS compania
+                   c.nombre AS compania,
+                   TIMESTAMPDIFF(MONTH, p.fec_ingreso, CURDATE()) AS antiguedad
             FROM personal p
             INNER JOIN rol_personal rp ON p.rol_personal_id = rp.id
             INNER JOIN compania c ON p.compania_id = c.id
@@ -143,7 +144,7 @@ export const getPersonalbyID = async (req, res) => {
             error: error.message
         });
     }
-}
+};
 
 
 export const createPersonal = async (req, res) => {

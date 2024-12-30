bf13e09e05353b29abbbd17f94a565c38e1c0e24
Benjuxx2303 <113943385+Benjuxx2303@users.noreply.github.com>
2024-12-29 21:04:45 -0300

    Add SSL configuration for database connection and update .gitignore

--- back/src/db.js
+++ back/src/db.js
@@ -1,10 +1,14 @@
 import {createPool} from "mysql2/promise";
 import { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_DATABASE } from "./config.js";
+import fs from "fs";
 
 export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     port: DB_PORT,
-    database: DB_DATABASE
+    database: DB_DATABASE,
+    ssl: {
+        ca: fs.readFileSync("./ca.pem")
+    }
 });
